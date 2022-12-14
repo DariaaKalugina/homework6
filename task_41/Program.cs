6cc2f656ec91c15d0a83902d95a5844f12e22ca0
Daria <macdaria1999@gmail.com>
2022-12-14 21:23:55 +0300

    Add task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+}
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
